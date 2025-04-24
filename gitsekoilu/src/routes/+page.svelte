ee94901fd1e29075b8c88cacc0eb3469e53ad645
Jeremia Vepsäläinen <Jeremia.vepsalainen@gmail.com>
2025-04-24 11:56:11 +0300

    pullattu develop branch

--- gitsekoilu/src/routes/+page.svelte
+++ gitsekoilu/src/routes/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+<<<<<<< HEAD
 	let teksti = $state('Jeremian tekemä muutos');
 </script>
 
 <main>
 	<h1>{teksti}</h1>
+=======
+</script>
+
+<main>
+	<h1 class="bg-amber-700">TERVE kaikille</h1>
+>>>>>>> 03c2a770ab5189c837722a0986b34250fab8d17e
 </main>
 
 <style>
